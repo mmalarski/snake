eabcfc225cad78a3c80fa4109225b898c8ba8ebf
mihau <mihau.malarski@gmail.com>
2021-03-18 21:56:51 +0100

    snake w dwoch kolorkach, jak zje banana to rosnie w tyl

---
+++ Fruit.py
@@ -0,0 +1,5 @@
+import Collectable
+
+class Fruit(Collectable):
+    def add_points(self, snake):
+        snake
--- Snake.py
+++ Snake.py
@@ -8,25 +8,29 @@ class SNAKE:
         self.cell_number = cell_number
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
-        self.new_block = False
 
     def set_direction(self, value):
         self.direction = value
 
     def draw_snake(self, screen):
+        i = 0
         for block in self.body:
             snake_rect = pygame.Rect(int(block.x * self.cell_size), int(block.y * self.cell_size),
                                      self.cell_size, self.cell_size)
-            pygame.draw.rect(screen, (183, 111, 122), snake_rect)
+            if i % 2 == 0:
+                pygame.draw.rect(screen, (183, 111, 122), snake_rect)
+            else:
+                pygame.draw.rect(screen, (150, 110, 120), snake_rect)
+            i += 1
 
     def move_snake(self):
-        if self.new_block:
-            body_copy = self.body[:]
-            self.new_block = False
-        else:
-            body_copy = self.body[:-1]
+        body_copy = self.body[:-1]
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
 
     def add_block(self):
-        self.new_block = True
+        last_vector = Vector2(self.body[-1].x - self.body[-2].x, self.body[-1].y - self.body[-2].y)
+        print(last_vector)
+        body_copy = self.body[:]
+        body_copy.insert(len(body_copy), body_copy[-1] + last_vector)
+        self.body = body_copy[:]
