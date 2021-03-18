c7a9d2bf4a8172c0ce43e00c9517140c1a3b61d5
mihau <mihau.malarski@gmail.com>
2021-03-18 13:27:11 +0100

    jakieś zabawy z rysowankiem

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-if __name__ == '__main__':
-    print('PyCharm')
+import pygame, sys
 
+pygame.init()
+screen = pygame.display.set_mode((1000, 800))
+clock = pygame.time.Clock()
+test_surface = pygame.Surface((100, 100))
+test_surface.fill((50, 60, 70))
+x_pos = 850
+y_pos = 650
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill((255, 255, 255))
+    screen.blit(test_surface, (x_pos, y_pos))
+    if x_pos != 0:
+        x_pos -= 1
+    if y_pos != 0:
+        y_pos -=1
+    pygame.display.update()
+    clock.tick(60)
