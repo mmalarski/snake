69322b0956224fb3536b72befd0efb17616123e5
mihau <mihau.malarski@gmail.com>
2021-03-18 18:24:56 +0100

    collectable randomise: w konstruktorze

--- Collectable.py
+++ Collectable.py
@@ -7,9 +7,7 @@ class COLLECTABLE:
     def __init__(self, cell_number, cell_size):
         self.cell_number = cell_number
         self.cell_size = cell_size
-        self.x = random.randint(0, self.cell_number - 1)
-        self.y = random.randint(0, self.cell_number - 1)
-        self.pos = Vector2(self.x, self.y)
+        self.randomise()
 
     def draw_collectable(self, screen):
         collectable_rect = pygame.Rect(int(self.pos.x * self.cell_size), int(self.pos.y * self.cell_size),
