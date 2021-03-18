0bdd1734eda16059acbdb4c52a39ed73e2178143
229871 Dyczka Monika <229871@edu.p.lodz.pl>
2021-03-18 17:38:01 +0100

    narysowanie kwadratu

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import sys
 import Collectable
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 800))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
 test_surface = pygame.Surface((100, 100))
 test_surface.fill((50, 60, 70))
 x_pos = 850
 y_pos = 650
-cell_size = 40
-cell_number = 100
+
 
 collectable = Collectable.COLLECTABLE(cell_number, cell_size)
+
 
 while True:
     for event in pygame.event.get():
@@ -20,5 +22,6 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill((175, 215, 70))
+    collectable.draw_collectable(screen)
     pygame.display.update()
     clock.tick(60)
