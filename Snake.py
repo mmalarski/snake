2b62e96f8ba3c1ad1aa3c796c2dcc34a753a3bac
mihau <mihau.malarski@gmail.com>
2021-03-18 18:58:42 +0100

    snake już nie może zawrócić w samego siebie (na początku gry też)

--- Snake.py
+++ Snake.py
@@ -6,7 +6,7 @@ class SNAKE:
     def __init__(self, cell_number, cell_size):
         self.cell_size = cell_size
         self.cell_number = cell_number
-        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
 
