7d0bb0da4ba164d552a34b9b5da5ae9d9986b673
mihau <mihau.malarski@gmail.com>
2021-04-27 19:49:22 +0200

    piperwsze okno działa

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import tkinter as tk
 import tkinter.font as font
 
 
-def playfunction(window):
-    window.destroy()
+def play_function(window_to_close):
+    window_to_close.destroy()
     snakelogic()
+
+
+def quit_function(window_to_close):
+    window_to_close.destroy()
 
 
 background = '#3ec965'
@@ -24,13 +28,15 @@ frame.place(in_=window, anchor='c', relx=.5, rely=.5)
 font0 = font.Font(size=30, family='Roboto')
 font1 = font.Font(size=20, weight='bold', family='Roboto')
 
-play_button = tk.Button(frame, command=lambda: playfunction(window), font=font0, text="PLAY", width=6, height=1, bg=buttoncolour, fg='lightblue')
+play_button = tk.Button(frame, command=lambda: play_function(window), font=font0, text="PLAY", width=6, height=1,
+                        bg=buttoncolour, fg='lightblue')
 play_button.pack(side=tk.TOP)
 
 highscore_label = tk.Label(frame, font=font1, text="HIGHSCORE:", bg=background, fg='#1c632f')
 highscore_label.pack(side=tk.TOP)
 
-quit_button = tk.Button(frame, font=font0, text="QUIT", width=6, height=1, bg=buttoncolour, fg='lightblue')
+quit_button = tk.Button(frame, command=lambda: quit_function(window), font=font0, text="QUIT", width=6, height=1,
+                        bg=buttoncolour, fg='lightblue')
 quit_button.pack(side=tk.TOP)
 
 window.mainloop()
