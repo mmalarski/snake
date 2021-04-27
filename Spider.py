c3f370bf24d61c2acbfc2ba7c7fe88cb4fefe64b
229871 Dyczka Monika <229871@edu.p.lodz.pl>
2021-04-27 17:01:01 +0200

    obrazki

--- Spider.py
+++ Spider.py
@@ -6,7 +6,7 @@ from EffectClass import EFFECT
 class SPIDER(EFFECT):
     def __init__(self, cell_number, cell_size, game):
         super().__init__(cell_number, cell_size, game)
-        self.image = pygame.transform.scale(pygame.image.load('Graphics/ant.svg').convert_alpha(), (cell_size, cell_size))
+        self.image = pygame.transform.scale(pygame.image.load('Graphics/apple.png').convert_alpha(), (cell_size, cell_size))
 
 
     def effect(self, game):
