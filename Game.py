1f6572ca15750b742acc403c82b0c98b7af0790a
229871 Dyczka Monika <229871@edu.p.lodz.pl>
2021-04-27 21:15:24 +0200

    zapisywanie high score

--- Game.py
+++ Game.py
@@ -72,10 +72,30 @@ class GAME:
         pygame.time.set_timer(SCREEN_UPDATE, speed)
         self.snake.speed = speed
 
+    def save_to_file(self):
+        try:
+            file = open('score.txt', 'w')
+            file.write(str(len(self.snake.body)-3))
+        except FileNotFoundError:
+            print("Nie podano pliku do odbioru.")
+
+    def read_file(self):
+        try:
+            file = open('score.txt', 'w')
+            score = int(file.read())
+            if score < len(self.snake.body) - 3:
+                self.save_to_file()
+        except FileNotFoundError:
+            print("Nie istnieje plik.")
+
+
     def game_over(self):
+        self.read_file()
         GUI.retry()
         sys.exit()
 
     def reload_bad_coll(self):
         self.collectable = random.choice(self.collectables)
         self.collectable.randomise()
+
+
